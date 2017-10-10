422c3f3a3cf41f73d5497e123569183b2413d396
silaghialecsandra <silaghialecsandra@yahoo.com>
2017-10-10 09:57:15 +0300

    pepene

---
+++ PepeneTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pepene
+{
+    [TestClass]
+    public class PepeneTest
+    {
+        [TestMethod]
+        public void Impartire()
+        {
+            Assert.AreEqual(true,Cantitate(8,4,4));
+            Assert.AreEqual(true, Cantitate(6, 4, 2));
+        }
+
+        bool Cantitate(int x,int p1,int p2)
+        {
+            if (x==p1+p2)
+            {
+                if (p1 % 2 == 0 && p2 % 2 == 0)
+                    return true;
+            }
+            return false;                  
+        }
+    }
+}
