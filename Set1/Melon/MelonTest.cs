4d516cc30e49c6583033b327b1a73f1a10b66a78
silaghialecsandra <silaghialecsandra@yahoo.com>
2017-10-18 19:06:57 +0300

    Melon
    Melon

---
+++ Set1/Melon/MelonTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Melon
+{
+    [TestClass]
+    public class MelonTest
+    {
+        [TestMethod]
+        public void Division()
+        {
+            Assert.AreEqual(true, Quantity(8, 4, 4));
+            Assert.AreEqual(true, Quantity(6, 4, 2));
+        }
+
+        bool Quantity(int kg, int part1, int part2)
+        {
+            if (kg == part1 + part2)         
+                return part1 % 2 == 0 && part2 % 2 == 0; 
+            return false;               
+
+        }
+    }
+}
