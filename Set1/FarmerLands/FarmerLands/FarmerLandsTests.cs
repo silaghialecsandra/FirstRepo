de14499b8be5e8759313c8877fdc1577d2216cdd
silaghialecsandra <silaghialecsandra@yahoo.com>
2017-11-02 09:47:04 +0200

    Calculate initial dimension

--- Set1/FarmerLands/FarmerLands/FarmerLandsTests.cs
+++ Set1/FarmerLands/FarmerLands/FarmerLandsTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*Un fermier are un teren pătrat. Pentru a-și extinde suprafața a mai cumpărat o parcelă alăturată.
+Noua parcelă are acceași lungime și are o lățime de 230m. Acum fermierul are un teren de 77ha (770.000mp).
+Ce dimensiune avea inițial terenul?*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FarmerLands
@@ -7,8 +11,16 @@ namespace FarmerLands
     public class FarmerLandsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InitialDimension()
         {
+            Assert.AreEqual(4, InitialDimensionFormula(1,8));
+        }
+
+        double InitialDimensionFormula(int width, long totalDimension)
+        {
+            double length;
+            length = Math.Pow((totalDimension / width), (1.0 / 3.0));
+            return length * length;
         }
     }
 }
