52c4f6b75a4af0a393b205db0941250a6c418486
silaghialecsandra <silaghialecsandra@yahoo.com>
2017-11-06 09:24:51 +0200

    Calculation of the penalty

--- Set1/MoneyDebts/MoneyDebts/MoneyDebtsTests.cs
+++ Set1/MoneyDebts/MoneyDebts/MoneyDebtsTests.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*Ai de plătit chiria lunară.Dar cu fiecare zi de întârziere trebuie să plătești o penalitate.
+Dacă ai întârziat:
+1-10 zile - plătești o penalitate de 2% din chirie/zi întârzire
+11-30 zile - plătești o penalitate de 5% din chirie/zi întârzire
+31-40 zile - plătești o penalitate de 10% din chirie/zi întârzire
+Dacă ști chiria și numărul de zile de întârziere, calculează suma totală de plată.*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MoneyDebts
@@ -7,8 +14,23 @@ namespace MoneyDebts
     public class MoneyDebtsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TotalPayment()
         {
+            Assert.AreEqual(1020, Penalty(1000, 1));
+            Assert.AreEqual(836.4, Penalty(820, 1));
+            Assert.AreEqual(1050, Penalty(1000, 11));
+            Assert.AreEqual(1100, Penalty(1000, 31));
+        }
+
+        public double Penalty(double Rent, int NrOfDays)
+        {
+            if ((NrOfDays >= 1) && (NrOfDays <= 10))
+                return Rent + (((2 * Rent) / 100) * NrOfDays);
+            if ((NrOfDays >= 11) && (NrOfDays <= 30))
+                return Rent + (((5 * Rent) / 100) * (NrOfDays - 10));
+            if ((NrOfDays >= 31) && (NrOfDays <= 40))
+                return Rent + (((10 * Rent) / 100) * (NrOfDays - 30));
+            return 0;
         }
     }
 }
