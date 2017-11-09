8a8d5c432cfd566b753cd9893c0326a52e0ce750
silaghialecsandra <silaghialecsandra@yahoo.com>
2017-11-09 09:31:39 +0200

    Add empty RomanDigits project

---
+++ Set1/RomanDigits/RomanDigits/RomanDigitsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanDigits
+{
+    [TestClass]
+    public class RomanDigitsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
