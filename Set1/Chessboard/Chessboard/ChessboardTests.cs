30cc7acc47b093d78fb06e9a1252535068b1a144
silaghialecsandra <silaghialecsandra@yahoo.com>
2017-11-22 11:41:36 +0200

    Add empty Chessboard project

---
+++ Set1/Chessboard/Chessboard/ChessboardTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chessboard
+{
+    [TestClass]
+    public class ChessboardTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
