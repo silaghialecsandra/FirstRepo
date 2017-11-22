58d024472609b678fdc3c9cf0df81cba4edeafe6
silaghialecsandra <silaghialecsandra@yahoo.com>
2017-11-22 12:02:44 +0200

    Squares formula

--- Set1/Chessboard/Chessboard/ChessboardTests.cs
+++ Set1/Chessboard/Chessboard/ChessboardTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿/*Calculează câte pătrate se pot forma pe o tablă de șah de dimensiunea de 8 x 8.*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Chessboard
@@ -7,8 +9,20 @@ namespace Chessboard
     public class ChessboardTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ChessboardSquares()
         {
+            Assert.AreEqual(204, SquaresFormula(8));
+        }
+
+        int SquaresFormula( int dimension)
+        {
+            int sum = 0;
+            while (dimension >= 1)
+            {
+                sum = sum + ((dimension * dimension) + ((dimension - 1) * (dimension - 1)));
+                dimension = dimension - 2;
+            }
+            return sum;
         }
     }
 }
