91298448b4f7623e7a7d1099b64a20c09f7dcb06
silaghialecsandra <silaghialecsandra@yahoo.com>
2017-11-27 10:46:58 +0200

    pangram formula

--- Set2/Pangram/Pangram/PangramTests.cs
+++ Set2/Pangram/Pangram/PangramTests.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿/*Un panagram e o frază care conține toate literele din alfabet (englezesc).
+Scrie un program care determină dacă o frază e o panagram.*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Pangram
 {
@@ -9,6 +13,15 @@ namespace Pangram
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(true, IsPangram("The quick brown fox jumps over the lazy dog"));
+            Assert.AreEqual(true, IsPangram("abcdefghijklmnopqrstuvwxyz"));
+            Assert.AreEqual(false, IsPangram("quick brown fox jumps over lazy dog"));
+        }
+
+        static bool IsPangram(string text)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            return alphabet.All(text.ToLower().Contains);
         }
     }
 }
