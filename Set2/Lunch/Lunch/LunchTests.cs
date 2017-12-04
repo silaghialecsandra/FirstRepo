6889a0d907d99704383ea7e0fe3649a8fcf3dc8b
silaghialecsandra <silaghialecsandra@yahoo.com>
2017-12-04 10:08:03 +0200

    Prefix formula

--- Set2/Lunch/Lunch/LunchTests.cs
+++ Set2/Lunch/Lunch/LunchTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*Tu și prietenul tău vă întâlniți întâmplător la restaurant.
+În timpul mesei de prânz din discuția avută cu el, afli că el ia prânzul la acel restaurant din 4 în 4 zile.
+Când te vei întâlni din nou cu el, dacă tu iei prânzul la restaurantul respectiv odată la 6 zile? */
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Lunch
@@ -7,8 +11,20 @@ namespace Lunch
     public class LunchTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Lunch()
         {
+        }
+
+        char LunchDays()
+        {
+            string daysOfWeek = " Monday Tuesday Wednesday Thursday Friday Saturday Sunday";
+            for(int i=1;i <= daysOfWeek.Length;i++)
+                for (int j = 1; j <= daysOfWeek.Length; j++)
+                {
+                    if ((4 * i) == (6 * j))
+                        return daysOfWeek[i];
+                }
+            return 'a';
         }
     }
 }
---
+++ Set2/Prefix/Prefix/PrefixTests.cs
@@ -0,0 +1,30 @@
+﻿/*Dacă ai două stringuri, găsește-le prefixul comun. Exemplu: "aaab" și "aaaabbaa" au prefixul comun "aaa" */
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prefix
+{
+    [TestClass]
+    public class PrefixTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("aaa",Prefix("aaab", "aaaabbaa"));
+        }
+
+        public char[] Prefix(string firstString, string secondString)
+        {
+            char[] prefix = new char[10];
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                if (firstString[i] == secondString[i])
+                    prefix[i] = firstString[i];
+                else
+                    break;
+            }
+            return prefix;
+        }
+    }
+}
